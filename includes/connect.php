6526f1b73bd1122e67ba1c85e8f8a3f307a520ea
bayanbel <57132702+bayanbel@users.noreply.github.com>
2021-01-09 20:51:30 +0200

    Initial commit

---
+++ includes/connect.php
@@ -0,0 +1,9 @@
+
+<?php
+	 $servername = "localhost";
+		 $username   = "root";
+		 $password   = "";
+		 $database   = "examination_system";
+		  $con;
+$con=mysqli_connect("localhost","root","","examination_system");
+?>
